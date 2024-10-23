cc3c3f91349031afeca3c72ef0e77ef6ac55b76e
Harry Qu <harry_qu00@yahoo.ca>
2024-10-23 16:05:10 -0700

    add room and name selection screen

--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"image"
+	"log"
 
+	"github.com/apache/pulsar-client-go/pulsar"
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/zehongharryqu/kingdom-of-heaven/assets"
 )
@@ -15,6 +19,13 @@ const (
 	ArtBigHeight  = 400
 	ArtBigWidth   = 300
 	ArtSmallWidth = 50
+)
+
+// game states
+const (
+	RoomName = "RoomName"
+	Lobby    = "Lobby"
+	Playing  = "Playing"
 )
 
 type Card struct {
@@ -51,27 +62,42 @@ func (h *Hand) In(x, y int) *ebiten.Image {
 }
 
 type Game struct {
-	h *Hand
+	state string
+	t     Typewriter
+	h     *Hand
 }
 
 func (g *Game) Update() error {
+	switch g.state {
+	case RoomName:
+		g.t.Update()
+		if g.t.confirmedName != "" && g.t.confirmedRoom != "" {
+			g.state = Playing // Todo: implement Lobby
+		}
+	}
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
-	g.h.Draw(screen)
-	cursorX, cursorY := ebiten.CursorPosition()
-	var displayX int
-	if cursorX > ScreenWidth/2 {
-		displayX = cursorX - ArtBigWidth
-	} else {
-		displayX = cursorX
+	switch g.state {
+	case RoomName:
+		g.t.Draw(screen)
+	case Playing:
+		g.h.Draw(screen)
+		cursorX, cursorY := ebiten.CursorPosition()
+		var displayX int
+		if cursorX > ScreenWidth/2 {
+			displayX = cursorX - ArtBigWidth
+		} else {
+			displayX = cursorX
+		}
+		if displayArt := g.h.In(cursorX, cursorY); displayArt != nil {
+			op := &ebiten.DrawImageOptions{}
+			op.GeoM.Translate(float64(displayX), 0)
+			screen.DrawImage(displayArt, op)
+		}
 	}
-	if displayArt := g.h.In(cursorX, cursorY); displayArt != nil {
-		op := &ebiten.DrawImageOptions{}
-		op.GeoM.Translate(float64(displayX), 0)
-		screen.DrawImage(displayArt, op)
-	}
+
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
@@ -79,12 +105,78 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeigh
 }
 
 func main() {
+	oauth := pulsar.NewAuthenticationOAuth2(map[string]string{
+		"type":       "client_credentials",
+		"issuerUrl":  "https://auth.streamnative.cloud/",
+		"audience":   "urn:sn:pulsar:o-hwa6o:kingdom-of-heaven-instance",
+		"privateKey": "file:///Users/harry/Downloads/o-hwa6o-harry.json", // Absolute path of your downloaded key file
+	})
+
+	client, err := pulsar.NewClient(pulsar.ClientOptions{
+		URL:            "pulsar+ssl://pc-de347430.gcp-shared-usce1.g.snio.cloud:6651",
+		Authentication: oauth,
+	})
+
+	if err != nil {
+		log.Fatalf("Could not instantiate Pulsar client: %v", err)
+	}
+
+	defer client.Close()
+
+	producer, err := client.CreateProducer(pulsar.ProducerOptions{
+		Topic: "persistent://public/default/koh-topic",
+	})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer producer.Close()
+
+	for i := 0; i < 10; i++ {
+		if msgId, err := producer.Send(context.Background(), &pulsar.ProducerMessage{
+			Payload: []byte(fmt.Sprintf("hello-%d", i)),
+		}); err != nil {
+			log.Fatal(err)
+		} else {
+			fmt.Printf("Published message: %v \n", msgId)
+		}
+	}
+
+	consumer, err := client.Subscribe(pulsar.ConsumerOptions{
+		Topic:                       "persistent://public/default/koh-topic",
+		SubscriptionName:            "test-sub",
+		SubscriptionInitialPosition: pulsar.SubscriptionPositionEarliest,
+	})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer consumer.Close()
+
+	for i := 0; i < 10; i++ {
+		msg, err := consumer.Receive(context.Background())
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Printf("Received message msgId: %v -- content: '%s'\n",
+			msg.ID(), string(msg.Payload()))
+
+		consumer.Ack(msg)
+	}
+
 	c := &Card{assets.CardBig, assets.CardSmall, nil}
 	h := &Hand{[]*Card{c, c, c, c, c}}
-	g := &Game{h}
+	g := &Game{RoomName, Typewriter{}, h}
 
-	err := ebiten.RunGame(g)
+	err = ebiten.RunGame(g)
 	if err != nil {
 		panic(err)
+	}
+
+	if err := consumer.Unsubscribe(); err != nil {
+		log.Fatal(err)
 	}
 }
---
+++ typewriter.go
@@ -0,0 +1,81 @@
+// the screen where the user picks their room and name
+package main
+
+import (
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+	"github.com/hajimehoshi/ebiten/v2/inpututil"
+)
+
+// repeatingKeyPressed return true when key is pressed considering the repeat state.
+func repeatingKeyPressed(key ebiten.Key) bool {
+	const (
+		delay    = 30
+		interval = 3
+	)
+	d := inpututil.KeyPressDuration(key)
+	if d == 1 {
+		return true
+	}
+	if d >= delay && (d-delay)%interval == 0 {
+		return true
+	}
+	return false
+}
+
+type Typewriter struct {
+	runes         []rune
+	currentText   string // what the user is typing
+	counter       int    // frame counter for blink
+	confirmedRoom string
+	confirmedName string
+}
+
+func (t *Typewriter) Update() error {
+	// Add runes that are input by the user by AppendInputChars.
+	// Note that AppendInputChars result changes every frame, so you need to call this
+	// every frame.
+	t.runes = ebiten.AppendInputChars(t.runes[:0])
+	t.currentText += string(t.runes)
+
+	// Adjust the string to be at most 10 characters
+	if len(t.currentText) > 10 {
+		t.currentText = t.currentText[:10]
+	}
+
+	// If the enter key is pressed, confirm the current text
+	if repeatingKeyPressed(ebiten.KeyEnter) || repeatingKeyPressed(ebiten.KeyNumpadEnter) {
+		if len(t.currentText) > 0 {
+			if t.confirmedRoom == "" {
+				t.confirmedRoom = t.currentText
+				t.currentText = ""
+			} else if t.confirmedName == "" {
+				t.confirmedName = t.currentText
+				t.currentText = ""
+			}
+		}
+	}
+
+	// If the backspace key is pressed, remove one character.
+	if repeatingKeyPressed(ebiten.KeyBackspace) {
+		if len(t.currentText) >= 1 {
+			t.currentText = t.currentText[:len(t.currentText)-1]
+		}
+	}
+
+	t.counter++
+	return nil
+}
+
+func (t *Typewriter) Draw(screen *ebiten.Image) {
+	// Blink the cursor.
+	currentTextDisplay := t.currentText
+	if t.counter%60 < 30 {
+		currentTextDisplay += "_"
+	}
+	message := "Please enter the name of the room you want to join:\n"
+	if t.confirmedRoom != "" {
+		message += t.confirmedRoom + "\nPlease enter your name:\n"
+	}
+	ebitenutil.DebugPrint(screen, message+currentTextDisplay)
+}

fb06600c4abc10e6842850e0c96688ee987cf466
Harry Qu <harry_qu00@yahoo.ca>
2024-10-23 20:01:15 -0700

    create pulsarclient class

--- main.go
+++ main.go
@@ -1,12 +1,8 @@
 package main
 
 import (
-	"context"
-	"fmt"
 	"image"
-	"log"
 
-	"github.com/apache/pulsar-client-go/pulsar"
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/zehongharryqu/kingdom-of-heaven/assets"
 )
@@ -64,6 +60,7 @@ func (h *Hand) In(x, y int) *ebiten.Image {
 type Game struct {
 	state string
 	t     Typewriter
+	pc    *PulsarClient
 	h     *Hand
 }
 
@@ -72,6 +69,7 @@ func (g *Game) Update() error {
 	case RoomName:
 		g.t.Update()
 		if g.t.confirmedName != "" && g.t.confirmedRoom != "" {
+			g.pc = newPulsarClient(g.t.confirmedRoom, g.t.confirmedName)
 			g.state = Playing // Todo: implement Lobby
 		}
 	}
@@ -105,78 +103,15 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeigh
 }
 
 func main() {
-	oauth := pulsar.NewAuthenticationOAuth2(map[string]string{
-		"type":       "client_credentials",
-		"issuerUrl":  "https://auth.streamnative.cloud/",
-		"audience":   "urn:sn:pulsar:o-hwa6o:kingdom-of-heaven-instance",
-		"privateKey": "file:///Users/harry/Downloads/o-hwa6o-harry.json", // Absolute path of your downloaded key file
-	})
-
-	client, err := pulsar.NewClient(pulsar.ClientOptions{
-		URL:            "pulsar+ssl://pc-de347430.gcp-shared-usce1.g.snio.cloud:6651",
-		Authentication: oauth,
-	})
-
-	if err != nil {
-		log.Fatalf("Could not instantiate Pulsar client: %v", err)
-	}
-
-	defer client.Close()
-
-	producer, err := client.CreateProducer(pulsar.ProducerOptions{
-		Topic: "persistent://public/default/koh-topic",
-	})
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	defer producer.Close()
-
-	for i := 0; i < 10; i++ {
-		if msgId, err := producer.Send(context.Background(), &pulsar.ProducerMessage{
-			Payload: []byte(fmt.Sprintf("hello-%d", i)),
-		}); err != nil {
-			log.Fatal(err)
-		} else {
-			fmt.Printf("Published message: %v \n", msgId)
-		}
-	}
-
-	consumer, err := client.Subscribe(pulsar.ConsumerOptions{
-		Topic:                       "persistent://public/default/koh-topic",
-		SubscriptionName:            "test-sub",
-		SubscriptionInitialPosition: pulsar.SubscriptionPositionEarliest,
-	})
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	defer consumer.Close()
-
-	for i := 0; i < 10; i++ {
-		msg, err := consumer.Receive(context.Background())
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		fmt.Printf("Received message msgId: %v -- content: '%s'\n",
-			msg.ID(), string(msg.Payload()))
-
-		consumer.Ack(msg)
-	}
 
 	c := &Card{assets.CardBig, assets.CardSmall, nil}
 	h := &Hand{[]*Card{c, c, c, c, c}}
-	g := &Game{RoomName, Typewriter{}, h}
+	g := &Game{RoomName, Typewriter{}, nil, h}
 
-	err = ebiten.RunGame(g)
+	err := ebiten.RunGame(g)
 	if err != nil {
 		panic(err)
 	}
 
-	if err := consumer.Unsubscribe(); err != nil {
-		log.Fatal(err)
-	}
+	g.pc.Close()
 }
---
+++ pulsar.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"log"
+
+	"github.com/apache/pulsar-client-go/pulsar"
+)
+
+type PulsarClient struct {
+	roomName, playerName string
+	client               pulsar.Client
+	producer             pulsar.Producer
+	consumer             pulsar.Consumer
+	// tableView            pulsar.TableView
+	// consumeCh            chan pulsar.ConsumerMessage
+	// // exclude type
+	// exclusiveObstacleConsumer pulsar.Consumer
+	// // to read the latest obstacle graph
+	// obstacleReader pulsar.Reader
+	// // subscribe the obstacle topic,
+	// closeCh chan struct{}
+}
+
+func (c *PulsarClient) Close() {
+	if err := c.consumer.Unsubscribe(); err != nil {
+		log.Fatal(err)
+	}
+	c.producer.Close()
+	c.consumer.Close()
+	c.client.Close()
+	// c.closeCh <- struct{}{}
+	// c.tableView.Close()
+	// close(c.closeCh)
+	// close(c.consumeCh)
+}
+
+func newPulsarClient(roomName, playerName string) *PulsarClient {
+	oauth := pulsar.NewAuthenticationOAuth2(map[string]string{
+		"type":       "client_credentials",
+		"issuerUrl":  "https://auth.streamnative.cloud/",
+		"audience":   "urn:sn:pulsar:o-hwa6o:kingdom-of-heaven-instance",
+		"privateKey": "file:///Users/harry/Downloads/o-hwa6o-harry.json", // Absolute path of your downloaded key file
+	})
+
+	client, err := pulsar.NewClient(pulsar.ClientOptions{
+		URL:            "pulsar+ssl://pc-de347430.gcp-shared-usce1.g.snio.cloud:6651",
+		Authentication: oauth,
+	})
+
+	if err != nil {
+		log.Fatalf("Could not instantiate Pulsar client: %v", err)
+	}
+
+	producer, err := client.CreateProducer(pulsar.ProducerOptions{
+		Topic: "persistent://public/default/koh-topic",
+	})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for i := 0; i < 10; i++ {
+		if msgId, err := producer.Send(context.Background(), &pulsar.ProducerMessage{
+			Payload: []byte(fmt.Sprintf("hello-%d", i)),
+		}); err != nil {
+			log.Fatal(err)
+		} else {
+			fmt.Printf("Published message: %v \n", msgId)
+		}
+	}
+
+	consumer, err := client.Subscribe(pulsar.ConsumerOptions{
+		Topic:                       "persistent://public/default/koh-topic",
+		SubscriptionName:            "test-sub",
+		SubscriptionInitialPosition: pulsar.SubscriptionPositionEarliest,
+	})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for i := 0; i < 10; i++ {
+		msg, err := consumer.Receive(context.Background())
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Printf("Received message msgId: %v -- content: '%s'\n",
+			msg.ID(), string(msg.Payload()))
+
+		consumer.Ack(msg)
+	}
+
+	return &PulsarClient{roomName, playerName, client, producer, consumer}
+}
